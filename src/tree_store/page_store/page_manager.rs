125e14e22cbfd2dff1c65e8fc8cb788a02b9ab68
VS <29.varun@gmail.com>
2026-04-30 21:08:57 +0530

    style: rustfmt import ordering in page_manager
    rustfmt expects unconditional imports before cfg-gated ones.

--- src/tree_store/page_store/page_manager.rs
+++ src/tree_store/page_store/page_manager.rs
@@ -1,6 +1,6 @@
+use crate::compat::Mutex;
 #[cfg(debug_assertions)]
 use crate::compat::{HashMap, HashSet};
-use crate::compat::Mutex;
 use crate::db::{ReadVerification, ReadVerificationAction, ReadVerificationCallback};
 use crate::transaction_tracker::TransactionId;
 use crate::transactions::{AllocatorStateKey, AllocatorStateTree, AllocatorStateTreeMut};
